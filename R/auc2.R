e32bcc44f8eb7545ff54aa00238559d35bc8d2cd
s341060 <moritz.schaaf@stud-mail.uni-wuerzburg.de>
2022-06-06 17:39:48 +0200

    added RP legacy functions

---
+++ R/auc2.R
@@ -0,0 +1,73 @@
+#' @title Area Under the Curve
+#'
+#' @description % This function computes areas under the curve (AUCs) for
+#' trajectory data with the 2D-coordinates px and py and a line, defined by
+#' slope a and intercept b. The arguments px and py may be scalars or vectors
+#' of the same length.
+#' Translation of the script given by Pfister et al. (2014) at
+#' https://doi.org/10.1016/j.cognition.2014.07.012
+#'
+#' @param x_vector vector of the x-coordinates of the executed trajectory
+#' @param y_vector vector of the y-coordinates of the executed trajectory
+#' @param slope slope of the ideal trajectory
+#' @param intercept intercept of the ideal trajectory
+#'
+#' @details AUCs are computed by dividing the area in triangular and square-shaped
+#' pieces and adding the up across the trajectory.
+#'
+#' @return Vector representing the cumulative AUC across course of the trajectory.
+#'
+#' @export
+#'
+
+auc2 <- function(x_vector, y_vector, slope, intercept) {
+
+  max_ad2_out <- rainR::max_ad2(x_vector, y_vector, slope, intercept)
+  ad <- max_ad2_out$ad
+  direction <- max_ad2_out$direction
+  linecoords <- max_ad2_out$linecoords
+
+  # allocate output vector
+  cum_auc <- rep(NA, length(ad))
+  cum_auc[1] <- 0
+
+  for (i in 2:length(ad)) {
+    # 0. Do not increase cum_auc if coordinates do not change.
+    if (linecoords$x[i] == linecoords$x[i - 1] &&
+        linecoords$y[i] == linecoords$y[i - 1]) {
+      cum_auc[i] <- cum_auc[i - 1]
+      next
+    }
+    # 1. Compute distance on line
+    ldist <- sqrt(((linecoords$x[i] - linecoords$x[i - 1]) ^ 2) +
+                   ((linecoords$y[i] - linecoords$y[i - 1]) ^ 2))
+    # 2. Check for line flips
+    if (direction[i] == direction[i - 1]) {
+      # If no flip is detected: Increment cum_auc (1) by
+      # rectangle (2) and triangle (3).
+      cum_auc[i] <- cum_auc[i - 1] +                              # (1)
+        direction[i] * ldist *
+        (min(ad[i], ad[i - 1]) +                                  # (2)
+           sign(ad[i] - ad[i - 1]) * (ad[i] - ad[i - 1]) / 2)     # (3)
+    } else {
+      # Add triangle or trapezoid (verschraenktes Trapez, Wiki)
+      # if trajectory hits or crosses ideal line.
+      if (direction[i] == 0) {
+        cum_auc[i] <- cum_auc[i - 1] + direction[i - 1] * ldist *
+          ((ad[i - 1] - ad[i]) / 2)
+
+      } else if (direction[i - 1] == 0) {
+        cum_auc[i] <- cum_auc[i - 1] + direction[i] * ldist *
+          ((ad[i] - ad[i - 1]) / 2)
+
+      } else {
+        cum_auc[i] <- cum_auc[i - 1] + direction[i] * ldist *
+          ((ad[i] - ad[i - 1]) / 2)
+
+      }
+    }
+  }
+
+  return(cum_auc)
+
+}
---
+++ R/max_ad2.R
@@ -0,0 +1,79 @@
+#' @title Maximum Absolute Deviation
+#'
+#' @description This function computes the absolute (Euclidean) distance of a
+#' point P (with the 2D-coordinates px and py) and a line, defined by slope a
+#' and intercept b. The arguments px and py may be scalars or vectors of the
+#' same length.
+#' Translation of the script given by Pfister et al. (2014) at
+#' https://doi.org/10.1016/j.cognition.2014.07.012
+#'
+#' @param x_vector vector of the x-coordinates of the executed trajectory
+#' @param y_vector vector of the y-coordinates of the executed trajectory
+#' @param slope slope of the ideal trajectory
+#' @param intercept intercept of the ideal trajectory
+#'
+#' @details
+#' The Euclidean distance d between P and the line defined by a and b can
+#' be computed as a function of the x coordinate of a point G(x,y) on the
+#' line (general case). To avoid confusion, I will denote px and py as p1
+#' and p2, respectively.
+#'
+#'    d(x) = sqrt( (p1-x)^2 + (p2-y)^2 )       | ()^2
+#'
+#'    d(x)^2 = (p1-x)^2 + (p2-y)^2             | solve brackets
+#'
+#'    d(x)^2 = p1^2 - 2*p1*x + x^2 + p2^2 - 2*p2*a*x - 2*p2*b + 2*a*x*b + a^2*x^2 + b^2
+#'
+#'    Rearranging to bring all terms including x's to the front...
+#'
+#'    d(x)^2 = (a^2+1)*x^2 + (2*a*b - 2*p1 - 2*p2*a)*x + p1^2 + p2^2 - 2*p2*b + b^2
+#'
+#'    Deriving this latter function helps to find the minimum
+#'    (absolute) distance between P and the line (note that none
+#'    of the terms in the second line of the above equation contains
+#'    any x's):
+#'
+#'    d(x)^2' = 2*(a^2+1)*x + 2*(a*b - p1 - p2*a)
+#'
+#'    d(x)^2' =(!)= 0
+#'
+#' The x coordinate corresponding to the global minimum is denoted as x0:
+#'
+#'    x0 = (p1 + p2*a - a*b) / (a^2+1)
+#'
+#' @return List containing 3 vectors. First, ad will be the absolute distances.
+#' Second, direction (1 = above the line, 0 = on the line, -1 = below the line).
+#' Third, linecoords (list with the vectors x and y for x0 and y0).
+#'
+#' @export
+#'
+
+
+
+max_ad2 <- function(x_vector, y_vector, slope, intercept) {
+  # input conversion to mirror variable names of RP
+  px <- x_vector
+  py <- y_vector
+  a <- slope
+  b <- intercept
+
+  # compute linecoords
+  x0 <- (px + py * a - a * b) / (a ^ 2 + 1)
+  y0 <- a * x0 + b
+  linecoords <- list()
+  linecoords$x <- x0
+  linecoords$y <- y0
+
+  # Finally, ad is computed as the simple Euclidean distance between the two
+  # points P(px,py) and G(x0,y0):
+  ad <- sqrt((px - x0) ^ 2 + (py - y0) ^ 2)
+
+  # Compute direction.
+  direction <- sign(py - (a * px + b))
+
+  return(list(
+    ad = ad,
+    direction = direction,
+    linecoords = linecoords
+  ))
+}
